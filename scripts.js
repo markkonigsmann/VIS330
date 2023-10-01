df9272cf4a302aa81df460071b4e6b384dec71ea
markkonigsmann <markkonigsmann@gmail.com>
2023-10-01 16:49:25 -0400

    more additions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
 <script>
-    // Your JavaScript code here
-    document.querySelector('h1').addEventListener('click', function() {
-        alert('Hello, World!');
-    });
+       // JavaScript to display the current time
+       function updateTime() {
+           const currentTimeElement = document.getElementById("current-time");
+           const now = new Date();
+           const hours = now.getHours().toString().padStart(2, "0");
+           const minutes = now.getMinutes().toString().padStart(2, "0");
+           const seconds = now.getSeconds().toString().padStart(2, "0");
+           currentTimeElement.textContent = `Current Time: ${hours}:${minutes}:${seconds}`;
+       }
+
+       // Update the time every second
+       setInterval(updateTime, 1000);
+
+       // Initial call to display the time
+       updateTime();
 </script>
