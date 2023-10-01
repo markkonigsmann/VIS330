921b4d0123f112408d83bb451ef7ec5406cfd691
markkonigsmann <markkonigsmann@gmail.com>
2023-10-01 17:14:15 -0400

    change script.js file

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-<script>
-       // JavaScript to display the current time
-       function updateTime() {
-           const currentTimeElement = document.getElementById("current-time");
-           const now = new Date();
-           const hours = now.getHours().toString().padStart(2, "0");
-           const minutes = now.getMinutes().toString().padStart(2, "0");
-           const seconds = now.getSeconds().toString().padStart(2, "0");
-           currentTimeElement.textContent = `Current Time: ${hours}:${minutes}:${seconds}`;
-       }
+// JavaScript to display the current time
+function updateTime() {
+   const currentTimeElement = document.getElementById("current-time");
+   const now = new Date();
+   const hours = now.getHours().toString().padStart(2, "0");
+   const minutes = now.getMinutes().toString().padStart(2, "0");
+   const seconds = now.getSeconds().toString().padStart(2, "0");
+   currentTimeElement.textContent = `Current Time: ${hours}:${minutes}:${seconds}`;
+}
 
-       // Update the time every second
-       setInterval(updateTime, 1000);
+// Update the time every second
+setInterval(updateTime, 1000);
 
-       // Initial call to display the time
-       updateTime();
-</script>
+// Initial call to display the time
+updateTime();
